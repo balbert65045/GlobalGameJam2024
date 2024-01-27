7cb93ad70b193d4a06657101b6cfeeb3cb9da042
debruler <debrulerij@vcu.edu>
2024-01-27 12:59:25 -0500

    adding streaks

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -9,6 +9,8 @@ public class Score : MonoBehaviour
     [SerializeField] TMP_Text scoreText;
     [SerializeField] float amountToChangeScoreBy = 50f;
     float score = 0;
+    int streak = 0;
+    float streakMultiplier = 10f;
     void Start()
     {
         InputArea[] inputAreas = FindObjectsOfType<InputArea>();
@@ -21,12 +23,14 @@ public class Score : MonoBehaviour
 
     void IncreaseScore()
     {
-        score += amountToChangeScoreBy;
+        score += amountToChangeScoreBy + streak*streakMultiplier;
+        streak++;
         scoreText.text = score.ToString();
     }
 
     void DecreaseScore()
     {
+        streak = 0;
         score -= amountToChangeScoreBy;
         scoreText.text = score.ToString();
     }
